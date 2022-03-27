0b7975b008f59a0db5deae62a879ecc0c58426ef
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-03-27 11:34:12 -0400

    added function for pulling data from db

--- components/DisplayBooks.js
+++ components/DisplayBooks.js
@@ -1,7 +1,34 @@
+import { db } from "../firebase";
+import { getDocs, collection } from "firebase/firestore";
+import { useEffect, useState } from "react"
+
 function DisplayBooks() {
+  const [item, setItem] = useState(null);
+  const [books, setBooks] = useState([]);
+
+  useEffect(()=>{
+    const booksRef = collection(db, "books");
+
+    const getBooks = getDocs(booksRef).then((snapshot)=>{
+      let result = [];
+      snapshot.docs.map((doc) => {
+        result.push({...doc.data(), id: doc.id});
+      })
+      setBooks(result);
+    })
+  },[])
   return (
     <div>
-      
+      {books && books.map(({id, img, title, price})=>(
+        <div key={id} className="">
+          <div className="relative">
+            <div><img src={img} alt="/" /></div>
+            <div><h2>{title}</h2></div>
+            <div><h3>{price}</h3></div>
+          </div>
+          
+        </div>
+      ))}
     </div>
   )
 }
