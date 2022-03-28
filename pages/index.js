eb220e413328a62457ff54845721c1ab9d628228
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-03-28 09:04:03 -0400

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,16 @@ import AddBooks from '../components/AddBooks'
 import DisplayBooks from '../components/DisplayBooks'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
       <Header />
       <DisplayBooks />
       <AddBooks />
