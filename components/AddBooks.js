dc0ee71669f4b9faf08b1d2c4a7861fef541772e
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-03-28 09:17:34 -0400

    fixed input values

--- components/AddBooks.js
+++ components/AddBooks.js
@@ -1,39 +1,58 @@
 import { useState } from "react";
-import { addDoc, collection } from "firebase/firestore"
+import { addDoc, collection } from "firebase/firestore";
 import { db } from "../firebase";
 
-
 function AddBooks() {
-  const [newTitle, setNewTitle] = useState('');
+  const [newTitle, setNewTitle] = useState("");
   const [newPrice, setNewPrice] = useState(0);
-  const [newImage, setNewImage] = useState('');
+  const [newImage, setNewImage] = useState("");
 
   const addBook = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     await addDoc(collection(db, "books"), {
       title: newTitle,
       price: Number(newPrice),
-      img: newImage
+      img: newImage,
     });
-    setNewTitle('')
+    setNewTitle("");
     setNewPrice(0);
-    setNewImage('')
-  }
+    setNewImage("");
+  };
 
   return (
     <div className="bg-slate-300">
       <div className="relative space-y-3">
         <form type="submit">
           <div className="p-5">
-            <input onChange={(e)=>setNewTitle(e.target.value)} placeholder="title" className="p-2 text-sm" type="text" />
+            <input
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              placeholder="title"
+              className="p-2 text-sm"
+              type="text"
+            />
           </div>
           <div className="p-5">
-            <input onChange={(e)=>setNewPrice(e.target.value)} placeholder="price" className="p-2 text-sm" type="number" />
+            <input
+              value={newPrice}
+              onChange={(e) => setNewPrice(e.target.value)}
+              placeholder="price"
+              className="p-2 text-sm"
+              type="number"
+            />
           </div>
           <div className="p-5">
-            <input onChange={(e)=>setNewImage(e.target.value)} placeholder="image url" className="p-2 text-sm" type="text" />
+            <input
+              value={newImage}
+              onChange={(e) => setNewImage(e.target.value)}
+              placeholder="image url"
+              className="p-2 text-sm"
+              type="text"
+            />
           </div>
-          <button onClick={addBook} className="m-5 p-2 text-white bg-slate-800">ADD</button>
+          <button onClick={addBook} className="m-5 p-2 text-white bg-slate-800">
+            ADD
+          </button>
         </form>
       </div>
     </div>
